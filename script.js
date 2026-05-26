69b98bc4017166eabad7024657f66d69a0fb8deb
V_A_L_S_O_R_A_Y <dragonkotik13@gmail.com>
2026-05-26 19:38:49 +0200

    hi

---
+++ script.js
@@ -0,0 +1,83 @@
+let inpt = document.querySelector('.d-gray')
+let str = ''
+
+
+document.querySelector('.btn-1').addEventListener('click',function(){
+    inpt.value += '1'
+})
+document.querySelector('.btn-2').addEventListener('click',function(){
+    inpt.value += '2'
+})
+document.querySelector('.btn-3').addEventListener('click',function(){
+    inpt.value += '3'
+})
+document.querySelector('.btn-4').addEventListener('click',function(){
+    inpt.value += '4'
+})
+document.querySelector('.btn-5').addEventListener('click',function(){
+    inpt.value += '5'
+})
+document.querySelector('.btn-6').addEventListener('click',function(){
+    inpt.value += '6'
+})
+document.querySelector('.btn-7').addEventListener('click',function(){
+    inpt.value += '7'
+})
+document.querySelector('.btn-8').addEventListener('click',function(){
+    inpt.value += '8'
+})
+document.querySelector('.btn-9').addEventListener('click',function(){
+    inpt.value += '9'
+})
+document.querySelector('.btn-0').addEventListener('click',function(){
+    inpt.value += '0'
+})
+
+
+
+document.querySelector('.btn-plus').addEventListener('click',function(){
+    inpt.value += '+'
+})
+document.querySelector('.btn-minus').addEventListener('click',function(){
+    inpt.value += '-'
+})
+document.querySelector('.btn-mult').addEventListener('click',function(){
+    inpt.value += '*'
+})
+document.querySelector('.btn-div').addEventListener('click',function(){
+    inpt.value += '/'
+})
+
+
+document.querySelector('.btn-equal').addEventListener('click',function(){
+    inp = inpt.value
+    if (!inp.includes('13*666/0')){
+        if (inp.includes('+')){
+            inp = inp.split('+')
+            inpt.value = +inp[0] + +inp[1]
+
+        }
+        if (inp.includes('-')){
+            inp = inp.split('-')
+            inpt.value = +inp[0] - +inp[1]
+        }
+        if (inp.includes('*')){
+            inp = inp.split('*')
+            inpt.value = +inp[0] * +inp[1]
+        }
+        if (inp.includes('/')){
+            inp = inp.split('/')
+            inpt.value = +inp[0] / +inp[1]
+        }
+    }
+    else{
+        inpt.value = ""
+        alert('death is upon us all')
+    }
+
+})
+document.querySelector('.btn-del').addEventListener('click',function(){
+    inpt.value = ""
+})
+
+
